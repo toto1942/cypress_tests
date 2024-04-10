90004b07d3aa77bbeaa941a0574947e96559e498
sesar <cvegbn@gmail.com>
2024-04-10 21:57:56 +0200

    1 test de login valido y 3 login no validos

--- cypress/e2e/heroku.cy.js
+++ cypress/e2e/heroku.cy.js
@@ -5,7 +5,42 @@ describe('template spec', () => {
     cy.get('#username').type('tomsmith')
     cy.get('#password').type('SuperSecretPassword!')
     cy.get('.fa').click()
-    cy.get('.fa').log( 'login exitoso')
+    cy.get('#flash').contains(' You logged into a secure area!')
+    
     
   }) 
- })
+
+  it('test de login no valido 1', () => {
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(21) > a').click()
+    cy.get('#username').type('tom')
+    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('.fa').click()
+    cy.get('#flash').contains(' You logged into a secure area!')
+    
+    
+  }) 
+
+  it('test de login  no valido 2', () => {
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(21) > a').click()
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('Super')
+    cy.get('.fa').click()
+    cy.get('#flash').contains(' You logged into a secure area!')
+    
+    
+  }) 
+
+  it('test de login no valido 3', () => {
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(21) > a').click()
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('.fa').click()
+    cy.get('#flash').contain('')
+    
+    
+  }) 
+
+})
